4a1f38239d089825a8901cea369ad27f22c4da34
Luis Felipe Paternina <luisfpaternina93@gmail.com>
2022-04-12 07:03:22 -0500

    update

--- sat_companies_quality/models/quality_alert.py
+++ sat_companies_quality/models/quality_alert.py
@@ -5,3 +5,30 @@ import logging
 
 class QualityAlert(models.Model):
     _inherit = 'quality.alert'
+
+    nonconformity_date = fields.Date(
+        string="Nonconformity date")
+    nonconformity_number = fields.Char(
+        string="Nonconformity number")
+    is_audit = fields.Boolean(
+        string="Is audit")
+    is_process = fields.Boolean(
+        string="Is process")
+    is_documentary = fields.Boolean(
+        string="Is documentary")
+    is_purchases_suppliers = fields.Boolean(
+        string="Purchases and suppliers")
+    is_complaints = fields.Boolean(
+        string="Is complaints")
+    nonconformity_description = fields.Text(
+        string="Description")
+    nonconformity_causes = fields.Text(
+        string="Causes")
+    detected_by = fields.Char(
+        string="Detected by")
+    detected_date = fields.Date(
+        string="Detected date")
+    immediate_solution = fields.Text(
+        string="Immediate solution")
+    is_indicators = fields.Boolean(
+        string="Is indicators")
