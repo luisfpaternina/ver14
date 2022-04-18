37184c7cadcf6c9ac20810b0a0dbedbc6b4a49fe
Luis Felipe Paternina <luisfpaternina93@gmail.com>
2022-04-18 07:40:09 -0500

    upd sat_companies_quality

--- sat_companies_quality/models/quality_alert.py
+++ sat_companies_quality/models/quality_alert.py
@@ -32,3 +32,31 @@ class QualityAlert(models.Model):
         string="Immediate solution")
     is_indicators = fields.Boolean(
         string="Is indicators")
+    user_execution_id = fields.Many2one(
+        'res.users',
+        string="Responsible execution")
+    execution_period = fields.Char(
+        string="Execution period")
+    closing_date = fields.Date(
+        string="Closing date")
+    risks_opportunities = fields.Boolean(
+        string="Risks and opportunities")
+    responsable_sign = fields.Binary(
+        string="Responsable sign")
+    is_corrective_action = fields.Boolean(
+        string="Is corrective action")
+    quality_responsable_sign = fields.Binary(
+        string="Quality responsable sign")
+    actions_carried_out =fields.Text(
+        string="Actions to be carried out")
+    user_carried_id = fields.Many2one(
+        'res.users',
+        string="Carried out responsable")
+    term = fields.Char(
+        string="Term")
+    monitoring_verification = fields.Text(
+        string="Monitoring and verification")
+    quality_responsable = fields.Binary(
+        string="Quality responsable sign")
+    quality_date = fields.Date(
+        string="Quality date")
