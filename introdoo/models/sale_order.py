fdd9f6c58ca75e9aaa329c9b7e959484103c3eef
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-04-18 20:55:14 -0500

    aded

--- introdoo/models/sale_order.py
+++ introdoo/models/sale_order.py
@@ -7,3 +7,6 @@ import logging
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    def print_report_introdoo(self):
+        return self.env.ref('introdoo.report_template_sale_order_introdoo').report_action(self)
